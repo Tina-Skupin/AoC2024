dd6cbe944772e084e4cd6b5d03db36d69c56367b
Tina-Skupin <tskupin32@gmail.com>
2024-12-03 09:24:50 +0100

    all conditions set

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func innerLogic(dataset []int) int {
 	safeReports := 0
-	isRising, isFalling := true, true
+	isRising, isFalling, isDistance := true, true, true
 
 	for i := 1; i < len(dataset); i++ {
 		if dataset[i-1] >= dataset[i] {
 			isRising = false
-		} else if dataset[i-1] <= dataset[i] {
+		} 
+		if dataset[i-1] <= dataset[i] {
 			isFalling = false
 		}
+		distance := math.Abs(float64(dataset[i] - dataset[i-1]))
+		if distance <1 || distance > 3{
+			isDistance = false
+		}
 	}
-	if isRising || isFalling {
+	if isRising || isFalling && isDistance{
 		safeReports++
 	}
 	return safeReports
@@ -23,7 +29,7 @@ func innerLogic(dataset []int) int {
 
 func main() {
 	// define dataset
-	dataset := []int{7, 6, 4, 2, 1}
+	dataset := []int{7, 6, 5, 2, 1}
 
 	//call the function
 	result := innerLogic(dataset)
